a3831835627b332f19f0aecb3a1689b063d53bb8
Luis Galvan <luis_galvan221@hotmail.com>
2025-03-29 13:12:13 -0600

    se agregaron rutas de navegacion en el navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,21 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="menu-container d-flex justify-content-between bg-dark text-light w-100">
       <div>
-        <h3 className="fs-3 p-1">Platillos</h3>
+        <Link to={"/platillos"} className="text-decoration-none text-light">
+          <h3 className="fs-3 p-1 ">Platillos</h3>
+        </Link>
       </div>
       <div>
-        <h3 className="fs-3 p-1">Bebidas</h3>
+        <Link to={"/bebidas"} className="text-decoration-none text-light">
+          <h3 className="fs-3 p-1">Bebidas</h3>
+        </Link>
       </div>
       <div>
-        <h3 className="fs-3 p-1">Postres</h3>
+        <Link to={"/postres"} className="text-decoration-none text-light">
+          <h3 className="fs-3 p-1">Postres</h3>
+        </Link>
       </div>
     </div>
   );
